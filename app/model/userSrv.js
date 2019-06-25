e766a96dbb7bc5a0b8451ae4f91c800d8fa6ec0c
LiatGidron <gidron.liat@gmail.com>
2019-06-25 20:33:47 +0300

    adding users.json and real (json) logic for login

--- app/model/userSrv.js
+++ app/model/userSrv.js
@@ -1,4 +1,4 @@
-app.factory("userSrv", function ($q) {
+app.factory("userSrv", function ($q, $http) {
     var activeUser = null;
 
     function User(plainUser) {
@@ -18,12 +18,21 @@ app.factory("userSrv", function ($q) {
     function login(email, pwd) {
         var async = $q.defer();
 
-        if (email === "gidron.liat@gmail.com" && pwd === "123") {
-            activeUser = new User({ id: 1, fname: "Liat", lname: "Gidron", email: "gidron.liat@gmail.com" });
-            async.resolve(activeUser);
-        } else {
-            async.reject(401);
-        }
+        activeUser = null;
+        $http.get("app/model/data/users.json").then(function(res) {
+            var users = res.data;
+            for (var i = 0; i < users.length && !activeUser; i++) {
+                if (email === users[i].email && pwd === users[i].pwd) {
+                    activeUser = new User(users[i]);
+                    async.resolve(activeUser);
+                } 
+            }
+            if (!activeUser) {
+                async.reject(401);
+            }
+        }, function(err) {
+            async.reject(err);
+        })
 
         return async.promise;
     }
