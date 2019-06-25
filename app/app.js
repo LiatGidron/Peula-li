78512d128ffd44aca5e0931062f7d516e0d7652f
LiatGidron <gidron.liat@gmail.com>
2019-06-25 09:11:40 +0300

    routing + home html + css to hebrew

--- app/app.js
+++ app/app.js
@@ -1,1 +1,17 @@
-var app = angular.module("peulaLiApp", []);
+var app = angular.module("peulaLiApp", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+    $routeProvider.when("/", {
+        templateUrl: "app/home/home.html"
+    }).when ("/login", {
+
+    }).when ("/signup", {
+        
+    }).when ("/activities", {
+        
+    }).when ("/newActivity", {
+        
+    }).when ("/myActivities", {
+        
+    })
+})
