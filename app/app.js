5f9667a9ae7b3ee9d78f7ece9a4d256b151f56d9
LiatGidron <gidron.liat@gmail.com>
2019-06-25 11:33:56 +0300

    login html and controller + navigation

--- app/app.js
+++ app/app.js
@@ -4,7 +4,8 @@ app.config(function($routeProvider) {
     $routeProvider.when("/", {
         templateUrl: "app/home/home.html"
     }).when ("/login", {
-
+        templateUrl: "app/login/login.html",
+        controller: "loginCtrl"
     }).when ("/signup", {
         
     }).when ("/activities", {
