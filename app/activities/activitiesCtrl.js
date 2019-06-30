ac3ce197c549115c61e6b1a585e286c18e2f4d99
LiatGidron <gidron.liat@gmail.com>
2019-06-30 23:49:00 +0300

    start working on filters

--- app/activities/activitiesCtrl.js
+++ app/activities/activitiesCtrl.js
@@ -5,4 +5,21 @@ app.controller("activitiesCtrl", function ($scope, activitiesSrv, $log) {
     }, function (err) {
         $log.error(err);
     });
+    $scope.query="";
+    $scope.filterBySubject = function(activity) {
+        if(activity.activityName.includes($scope.query) || activity.activityTags.includes($scope.query)) {
+            return true;
+        } else {
+            return false; 
+        }
+    }
+    $scope.length="";
+    $scope.filterByLength = function(activity){
+        if(activity.activityLength === $scope.length) {
+            return true;
+        } else {
+            return false; 
+        }
+    }
+   
 })
