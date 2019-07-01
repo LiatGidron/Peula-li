572a9651aa619a139d2c38e627c592b40ab43bc8
LiatGidron <gidron.liat@gmail.com>
2019-07-01 11:31:34 +0300

    preparing subject, location, length and age filters

--- app/activities/activitiesCtrl.js
+++ app/activities/activitiesCtrl.js
@@ -13,13 +13,37 @@ app.controller("activitiesCtrl", function ($scope, activitiesSrv, $log) {
             return false; 
         }
     }
-    $scope.length="";
+
     $scope.filterByLength = function(activity){
-        if(activity.activityLength === $scope.length) {
+        if ($scope.length===undefined) {
+            return true
+        } else if(activity.activityLength === $scope.length) {
             return true;
         } else {
             return false; 
         }
     }
-   
+    // $scope.location = "";
+    $scope.filterByLocation = function(activity){
+        if ($scope.location===undefined) {
+            return true
+        } else if(activity.activityLocation===$scope.location) {
+            return true;
+        } else {
+            return false; 
+        }
+    }
+    $scope.ages = [];
+    $scope.includeAge = function(age) {
+        $scope.ages.push(age); 
+    }
+   $scope.filterByAge = function(activity) {
+    if ($scope.ages=="") {
+        return true
+    } else if ($scope.ages.indexOf(activity.activityAge)!==-1) {
+           return true;
+       } else {
+           return false;
+       }
+   }
 })
