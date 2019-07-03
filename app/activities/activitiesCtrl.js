6e4e76e213fb212a009ff345affe3c2e2c5a54f1
LiatGidron <gidron.liat@gmail.com>
2019-07-03 13:04:43 +0300

    fixing the checkbox logic when unselected

--- app/activities/activitiesCtrl.js
+++ app/activities/activitiesCtrl.js
@@ -6,77 +6,86 @@ app.controller("activitiesCtrl", function ($scope, activitiesSrv, $log, userSrv)
         $log.error(err);
     });
 
-    $scope.query="";
-    $scope.filterBySubject = function(activity) {
-        if(activity.activityName.includes($scope.query) || activity.activityTags.includes($scope.query)) {
+    $scope.query = "";
+    $scope.filterBySubject = function (activity) {
+        if (activity.activityName.includes($scope.query) || activity.activityTags.includes($scope.query)) {
             return true;
         } else {
-            return false; 
+            return false;
         }
     }
 
-    $scope.filterByLength = function(activity){
-        if ($scope.length===undefined) {
+    $scope.filterByLength = function (activity) {
+        if ($scope.length === undefined) {
             return true
-        } else if(activity.activityLength === $scope.length) {
+        } else if (activity.activityLength === $scope.length) {
             return true;
         } else {
-            return false; 
+            return false;
         }
     }
-    $scope.filterByLocation = function(activity){
-        if ($scope.location===undefined) {
+    $scope.filterByLocation = function (activity) {
+        if ($scope.location === undefined) {
             return true
-        } else if(activity.activityLocation===$scope.location) {
+        } else if (activity.activityLocation === $scope.location) {
             return true;
         } else {
-            return false; 
+            return false;
         }
     }
     $scope.ages = [];
-    $scope.includeAge = function(age) {
-        $scope.ages.push(age); 
+    $scope.includeAge = function (age) {
+        if ($scope.ages.indexOf(age) !== -1) {
+            $scope.ages.splice($scope.ages.indexOf(age), 1);
+        } else {
+            $scope.ages.push(age);
+        }
     }
-   $scope.filterByAge = function(activity) {
-    if ($scope.ages=="") {
-        return true
-    } else if ($scope.ages.indexOf(activity.activityAge)!==-1) {
-           return true;
-       } else {
-           return false;
-       }
-   }
-
-   $scope.levelsOfActive = [];
-   $scope.includeLevelOfActive = function(level) {
-       $scope.levelsOfActive.push(level); 
-   }
-  $scope.filterByLevelOfActive = function(activity) {
-   if ($scope.levelsOfActive=="") {
-       return true
-   } else if ($scope.levelsOfActive.indexOf(activity.levelOfActive)!==-1) {
-          return true;
-      } else {
-          return false;
-      }
-  }
-
-  $scope.filterByYouthMovement = function(activity){
-    if ($scope.youthMovement===undefined) {
-        return true
-    } else if(activity.youthMovement === $scope.youthMovement) {
-        return true;
-    } else {
-        return false; 
-    }
+    $scope.filterByAge = function (activity) {
+        if ($scope.ages == "") {
+            return true
+        } else if ($scope.ages.indexOf(activity.activityAge) !== -1) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    $scope.clearFilter = function() {
+    $scope.levelsOfActive = [];
+    $scope.includeLevelOfActive = function (level) {
+        if ($scope.levelsOfActive.indexOf(level) !== -1) {
+            $scope.levelsOfActive.splice($scope.levelsOfActive.indexOf(level), 1);
+        } else {
+            $scope.levelsOfActive.push(level);
+        }
+    }
+
+    $scope.filterByLevelOfActive = function (activity) {
+        if ($scope.levelsOfActive == "") {
+            return true
+        } else if ($scope.levelsOfActive.indexOf(activity.levelOfActive) !== -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    $scope.filterByYouthMovement = function (activity) {
+        if ($scope.youthMovement === undefined) {
+            return true
+        } else if (activity.youthMovement === $scope.youthMovement) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    $scope.clearFilter = function () {
         $scope.query = "";
-        $scope.length= undefined;
-        $scope.location=undefined; 
-        $scope.ages=[];
-        $scope.levelsOfActive=[];
-        $scope.youthMovement=undefined;
-      };
+        $scope.length = undefined;
+        $scope.location = undefined;
+        $scope.ages = [];
+        $scope.levelsOfActive = [];
+        $scope.youthMovement = undefined;
+    };
 })
